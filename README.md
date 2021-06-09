c863a5650f84ac8ba690c3f13608957b5a4801b8
limunosekai <powerlsh0103@gmail.com>
2021-06-09 17:31:11 +0900

    ADD README

---
+++ README.md
@@ -0,0 +1,25 @@
+## HOLD-TO-SHIFT-CHECK
+
+[Go to WEB](https://limunosekai.github.io/hold-to-shift-and-check/)
+
+<br>
+
+### ✨ Main Logic
+
+![1](./img/K-008.png)
+
+<br>
+
+<br>
+
+### ✨ Screen Shot
+
+![2](./img/K-007.png)
+
+<br>
+
+<br>
+
+---
+
+Source : [JAVASCRIPT 30](https://javascript30.com/)
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,23 @@
 const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
 
-checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
+checkboxes.forEach((checkbox) =>
+  checkbox.addEventListener('click', handleCheck)
+);
 
 let lastChecked; //마지막에 click한 checkbox를 기록하는 변수
 
 function handleCheck(e) {
   let inBetween = false; //매 클릭 마다 처음 시작은 false
-  if (e.shiftKey && this.checked) { //shift를 누른채 체크
-    checkboxes.forEach(checkbox => {
-      if (checkbox === this || checkbox === lastChecked) { //루프를 돌다가 last 혹은 this를 만나면 inBetween 값이 전환
+  if (e.shiftKey && this.checked) {
+    //shift를 누른채 체크
+    checkboxes.forEach((checkbox) => {
+      //루프를 돌다가 last 혹은 this를 만나면 inBetween 값이 전환
+      if (checkbox === this || checkbox === lastChecked) {
         inBetween = !inBetween;
       }
 
-      if (inBetween) {                 //inBetween이 true인 값은 모두 checked 상태가 된다
+      if (inBetween) {
+        //inBetween이 true인 값은 모두 checked 상태가 된다
         checkbox.checked = true;
       }
     });
